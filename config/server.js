fe1bfd7b01b7e18d600eaed5a8eea23c0c41e9ac
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-02-20 15:34:58 -0600

    Backend API Call Made

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = {
+  APPLICATION_ID:
+    process.env.APPLICATION_ID ||
+    "ddef16e1c5a4041464a9dfd8ff217e9e11a84a43c90afd3c7b638a3ae2d94ee8",
+  SECRET:
+    process.env.SECRET ||
+    "265e7f7afb11021876502516208ab47b833120c8d9b341490dec634a65f1a599",
+  CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000"
+};
